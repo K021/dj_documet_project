f167864d321053b40515c13ee31c7d75a44835f9
Kim Jooeon <joo2theeon@gmail.com>
2017-10-10 16:35:23 +0900

    many_to_many 쪼개기

---
+++ dj_doc/model/models/many_to_many/__init__.py
@@ -0,0 +1,3 @@
+from .basic import *
+from .self_type import *
+from .symmetrical_false import *
---
+++ dj_doc/model/models/many_to_many/symmetrical_false.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+
+__all__ = (
+    'InstagramUser',
+)
+
+
+class InstagramUser(models.Model):
+    name = models.CharField(max_length=50)
+    following = models.ManyToManyField('self', blank=True, symmetrical=False)
+
+    def __str__(self):
+        return self.name
