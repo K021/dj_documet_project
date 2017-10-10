14927284b523de247036789725df33be1f15ef18
Kim Jooeon <joo2theeon@gmail.com>
2017-10-10 13:58:58 +0900

    update: model(User)

--- dj_doc/model/models.py
+++ dj_doc/model/models.py
@@ -14,7 +14,18 @@ class Person(models.Model):
 class Manufacturer(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 
 class Car(models.Model):
     name = models.CharField(max_length=50, blank=True)
     manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.name} of {self.manufacturer.name}'
+
+
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    teacher = models.ForeignKey('self', on_delete=models.SET_NULL, null=True)
