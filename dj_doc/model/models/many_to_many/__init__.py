a4db45f55d5cecc815219ca54926f2d65aa73f06
Kim Jooeon <joo2theeon@gmail.com>
2017-10-11 11:55:00 +0900

    update: model through field, related_name 추가

--- dj_doc/model/models/many_to_many/__init__.py
+++ dj_doc/model/models/many_to_many/__init__.py
@@ -1,3 +1,4 @@
 from .basic import *
 from .self_type import *
 from .symmetrical_false import *
+from .intermediate import *
---
+++ dj_doc/model/models/many_to_many/intermediate.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+__all__ = (
+    'Idol',
+    'Group',
+    'Membership',
+)
+
+
+class Idol(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=50)
+    debut_date = models.DateField(null=True, blank=True)
+    members = models.ManyToManyField(
+        Idol,
+        through='Membership',
+        through_fields=('group', 'idol'),
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Membership(models.Model):
+    idol = models.ForeignKey(Idol, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    recommender = models.ForeignKey(
+        Idol,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='recommended_membership_set',
+    )
+    joined_date = models.DateField(null=True, blank=True)
+    is_active = models.BooleanField()
+
+    def __str__(self):
+        is_active = '활동 중' if self.is_active else '비활동'
+        return f'{self.idol.name} ({self.group.name} | {is_active})'
