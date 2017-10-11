aa74ea7714ed145a4a4aa4c7659318cefdd1cff3
Kim Jooeon <joo2theeon@gmail.com>
2017-10-11 16:04:22 +0900

    update: inheritance model, School, CommonInfo

--- dj_doc/inheritance/admin.py
+++ dj_doc/inheritance/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 
 from .models import *
 
+admin.site.register(School)
 admin.site.register(Student)
 admin.site.register(Teacher)
--- dj_doc/inheritance/models.py
+++ dj_doc/inheritance/models.py
@@ -1,18 +1,28 @@
 from django.db import models
 
 __all__ = (
+    'School',
     'CommonInfo',
     'Student',
     'Teacher',
 )
 
 
+class School(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
 class CommonInfo(models.Model):
+    school = models.ForeignKey(School, null=True, blank=True)
     name = models.CharField(max_length=100)
     age = models.PositiveIntegerField()
 
     class Meta:
         abstract = True
+        ordering = ['name']
 
 
 class Student(CommonInfo):
