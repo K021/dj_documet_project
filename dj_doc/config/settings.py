d59b4e7a179adfb0ee25df243be61a0588658dcb
Kim Jooeon <joo2theeon@gmail.com>
2017-10-12 13:45:46 +0900

    init: making_queries

--- dj_doc/config/settings.py
+++ dj_doc/config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
     'model',
     'inheritance',
+    'making_queries',
 ]
 
 MIDDLEWARE = [
--- dj_doc/inheritance/admin.py
+++ dj_doc/inheritance/admin.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 
 from .models import *
 
+# class ChampionAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'champion_type', 'rank')
+#     list_
+
 admin.site.register(School)
 admin.site.register(Student)
 admin.site.register(Teacher)
 admin.site.register(Place)
 admin.site.register(Restaurant)
+admin.site.register(Champion)
